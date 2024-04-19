c0e0e3020995f871e04385b08912bdae53ade1ab
guri805 <gursangamsingh2@gmail.com>
2024-04-19 13:46:11 +0530

    second commit

--- launch.java
+++ launch.java
@@ -1,5 +1,11 @@
 public class launch {
+    public static void add(){
+        int a=10,b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
     public static void main(String[] args) {
         System.out.println("explore github");
+
     }
 }
